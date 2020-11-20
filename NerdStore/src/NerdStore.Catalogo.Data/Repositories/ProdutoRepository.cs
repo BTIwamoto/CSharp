9b649cf6179a2334902bdbd972c10baac1916ab4
Bruno Ivamoto <bruno.ivamoto@actuar.com>
2020-11-19 23:51:07 -0300

    Implementação da camada de Data

---
+++ NerdStore/src/NerdStore.Catalogo.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NerdStore.Catalogo.Data.Context;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Catalogo.Domain.Repositories;
+using NerdStore.Core.Data;
+
+namespace NerdStore.Catalogo.Data.Repositories
+{
+    public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        public ProdutoRepository(CatalogoContext context) : base(context)
+        {
+            
+        }
+
+        public async Task<IEnumerable<Produto>> ObterTodos()
+        {
+            return await Context.Produtos.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Produto> ObterPorId(Guid id)
+        {
+            return await Context.Produtos.AsNoTracking().FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterPorCategoria(int codigo)
+        {
+            return await Context.Produtos.AsNoTracking().Include(p => p.Categoria).Where(c => c.Categoria.Codigo == codigo).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Categoria>> ObterCategorias()
+        {
+            return await Context.Categorias.AsNoTracking().ToListAsync();
+        }
+    }
+}
---
+++ NerdStore/src/NerdStore.Core/Data/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+
+namespace NerdStore.Core.Data
+{
+    public interface IRepository<T> : IDisposable where T : Entity
+    {
+        IUnitOfWork UnitOfWkOfWork { get; }
+
+        void Adicionar(T obj);
+
+        void Atualizar(T obj);
+    }
+}
